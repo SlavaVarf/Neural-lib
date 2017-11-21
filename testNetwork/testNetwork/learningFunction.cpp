2d4bd994126b927fbf61da20a1afd19814e47699
Вячеслав Варфоломеев <svix55@ya.ru>
2017-11-21 20:22:29 +0500

    v1.0.1

---
+++ testNetwork/testNetwork/learningFunction.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+//#include "learningFunction.h"
+/*
+
+double weigts_delta(double actual,double expected) {
+double error = actual - expected;                 //2.1
+double weights_delta = error*sweights.weights[CountOfNeurons - 1][CountOfNeurons - 1];
+return weigts_delta;
+}
+*/
---
+++ testNetwork/testNetwork/learningFunction.h
@@ -0,0 +1,4 @@
+#pragma once
+
+//double weigts_delta(double actual, double expected);
+//сюда вписать прототипы функций из learningFunction.cpp
