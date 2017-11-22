74f8b48044f306e3d4f678aa4142309dc4e864be
julia.osintseva15 <julia.osintseva15@gmail.com>
2017-11-22 18:46:23 +0500

    learning formuls

--- testNetwork/testNetwork/learningFunction.cpp
+++ testNetwork/testNetwork/learningFunction.cpp
@@ -1,19 +1,23 @@
 #include "stdafx.h"
 #include "learningFunction.h"
 
-double gotWeights(double weight) {
-	return weight;
-}
-double setWeigth(double weight, double neuron,double delta, double lr) {
-double a = (weight - neuron*delta*lr);
-return a;
+double wDelta;
+double learningRate;
+void weights_delta(double actual,double expected) {
+	double error = actual - expected;					//2.1
+	double sigmoidDX = actual*(1 - actual);				//2.2*
+     wDelta = error*sigmoidDX;							//2.2
 }
 
-/*
-
-double weigts_delta(double actual,double expected) {
-double error = actual - expected;                 //2.1
-double weights_delta = error*sweights.weights[CountOfNeurons - 1][CountOfNeurons - 1];
-return weigts_delta;
+double new_weight(double valueOfThisNeu,double valueOfPrevNeu) {
+	
+	double new_weight = valueOfThisNeu - valueOfPrevNeu*wDelta*learningRate; //2.3
+	return new_weight;
 }
-*/
+
+double new_valueOfNeu(double valueOfNeu,double weight) {
+	
+	double error = weight*wDelta;                                 //2.4
+	double newValue = valueOfNeu - weight*wDelta*learningRate;    //2.5
+	return newValue;
+}
--- testNetwork/testNetwork/learningFunction.h
+++ testNetwork/testNetwork/learningFunction.h
@@ -1,6 +1,11 @@
 #pragma once
 
-double gotWeights(double weight);
-double setWeigth(double weight, double neuron, double delta, double lr);
+void weights_delta(double actual, double expected);
+double new_weight(double valueOfThisNeu, double valueOfPrevNeu);
+double new_valueOfNeu(double valueOfNeu, double weight);
+
+
+
+ 
 	//double weigts_delta(double actual, double expected);
 //сюда вписать прототипы функций из learningFunction.cpp
