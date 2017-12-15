ec5f0ebfa7651f7196da9f116142c6c7ec8f4318
Вячеслав Варфоломеев <svix55@ya.ru>
2017-12-15 22:33:05 +0500

    Learning confirm

--- testNetwork/testNetwork/matrix.cpp
+++ testNetwork/testNetwork/matrix.cpp
@@ -48,7 +48,7 @@ int neuronsCounter(int* neuronsPerLayer, int layersNum) {  //ф-ия,считающая и во
 	return neuronsNum;
 }
 
-double ** forwardWay(double** weights, int* neuronsPerLayer, int layersNum, double expected) {     //прямой проход
+double ** FBWay(double** weights, int* neuronsPerLayer, int layersNum, double expected) {     //прямой проход
 	int *layerStart = new int[layersNum];
 	layerStart[0] = 0;
 	for (int i = 1; i < layersNum; i++) {
@@ -61,7 +61,24 @@ double ** forwardWay(double** weights, int* neuronsPerLayer, int layersNum, doub
 		}
 	}
 	//backWay(weights, neuronsPerLayer, layersNum, expected, layerStart, 0.1);
-	backWay(weights, weights[neuronsPerLayer[2]][neuronsPerLayer[2]], expected, layersNum, layerStart);
+	backWay(weights, weights[layerStart[layersNum - 1]][layerStart[layersNum - 1]], expected, layersNum, layerStart);
+	return weights;
+}
+
+double ** solution(double** weights, int* neuronsPerLayer, int layersNum) {     //прямой проход
+	int *layerStart = new int[layersNum];
+	layerStart[0] = 0;
+	for (int i = 1; i < layersNum; i++) {
+		layerStart[i] = layerStart[i - 1] + neuronsPerLayer[i - 1];
+		for (int k = 0; k < neuronsPerLayer[i]; k++) {
+			weights[layerStart[i] + k][layerStart[i] + k] = 0;
+			for (int j = 0; j < neuronsPerLayer[i - 1]; j++)
+				weights[layerStart[i] + k][layerStart[i] + k] += weights[layerStart[i - 1] + j][layerStart[i - 1] + j] * weights[neuronsPerLayer[i - 1] + k][layerStart[i - 1] + j];
+			weights[layerStart[i] + k][layerStart[i] + k] = sigm(weights[layerStart[i] + k][layerStart[i] + k]);
+		}
+	}
+	for (int i = layerStart[layersNum - 1]; i < layerStart[layersNum - 1] + neuronsPerLayer[layersNum - 1]; i++)
+		cout << "weights[" << i << "][" << i << "]=" << answer(weights[i][i]);
 	return weights;
 }
 
@@ -74,12 +91,13 @@ double **backWay(double**weights, double actual, double expected, int layersNum,
 
 	double error;
 
-	for (int i = layerStart[layersNum - 2]; i < layerStart[layersNum - 1]; i++)
+	for (int i = layerStart[layersNum - 2]; i < layerStart[layersNum - 1]; i++) {
 		error = error_of_neu(weights[layerStart[layersNum - 1]][i]);
-	for (int k = layersNum - 2; k>0; k--)
-		for (int m = layerStart[k]; m<layerStart[k + 1]; m++)
-			for (int j = layerStart[k - 1]; j<layerStart[k]; j++)
-				new_weight(weights[m][m], weights[m][j], error);
+		for (int k = layersNum - 2; k > 0; k--)
+			for (int m = layerStart[k]; m < layerStart[k + 1]; m++)
+				for (int j = layerStart[k - 1]; j < layerStart[k]; j++)
+					weights[m][j] = new_weight(weights[m][m], weights[m][j], error);
+	}
 	return weights;
 
 }
@@ -96,9 +114,7 @@ return weights;																						//возвращаем матрицу в forwardWay
 */
 
 double sigm(double x) {
-	cout << "prev " << x << "\n";
 	double sigm = (1 / (1 + exp(-x)));
-	cout << "aft " << sigm << "\n";
 	return sigm;
 }
 
--- testNetwork/testNetwork/matrix.h
+++ testNetwork/testNetwork/matrix.h
@@ -3,8 +3,8 @@
 int neuronsCounter(int* neuronsPerLayer, int layersNum);
 double ** matrixCreation(int* neuronsPerLayer, int layersNum);
 void writeToFile(string fileName, double **matrix, int neuronsNum);
-double ** forwardWay(double** weights, int* neuronsPerLayer, int layersNum, double expected);
-//double ** backWay(double** weights, int* neuronsPerLayer, int layersNum, double expected, int* layerStart, double learningRate);
+double ** solution(double** weights, int* neuronsPerLayer, int layersNum);
+double ** FBWay(double** weights, int* neuronsPerLayer, int layersNum, double expected);
 double **backWay(double**weights, double actual, double expected, int layersNum, int* layerStart);
 double sigm(double x);
 double answer(double x);
